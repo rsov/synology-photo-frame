db063d33e69b32d6f2fbfbc857eda07318fdd6b2
Ruslan <ruzzzlan@hotmail.com>
2026-02-10 21:38:28 +1100

    Initial commit, WIP. image shows up half chopped

---
+++ README.md
@@ -0,0 +1,11 @@
+# What is
+
+ReTerminal E1002 color display picture frame to view photos from Synology Photos album
+
+
+# Notes
+
+See https://github.com/Frans-Willem/reterminal_e100x for stuff
+
+
+Data https://files.seeedstudio.com/wiki/reterminal_e10xx/res/202004321_reTerminal_E1002_V1.0_SCH_250805.pdf
---
+++ src/bin/main.rs
@@ -0,0 +1,269 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use defmt::{info, println};
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use embedded_graphics::image::{Image, ImageRaw};
+use embedded_graphics::pixelcolor::Rgb888;
+use embedded_graphics::prelude::*;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use epd_waveshare::color::HexColor;
+use epd_waveshare::epd7in3e::{Display7in3e, Epd7in3e};
+use epd_waveshare::prelude::WaveshareDisplay;
+use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Input, InputConfig, Pull};
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::spi::master::Spi;
+use esp_hal::timer::timg::TimerGroup;
+use reqwless::client::TlsConfig;
+use zune_jpeg::JpegDecoder;
+use zune_jpeg::zune_core::bytestream::ZCursor;
+use {esp_backtrace as _, esp_println as _};
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.2.0
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
+    esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
+
+    info!("Embassy initialized!");
+
+    let radio_init = RADIO_CONTROLLER
+        .init(esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller"));
+    let (mut wifi_controller, interfaces) =
+        esp_radio::wifi::new(radio_init, peripherals.WIFI, Default::default())
+            .expect("Failed to initialize Wi-Fi controller");
+
+    const SSID: &str = env!("WIFI_SSID");
+    const PASSWORD: &str = env!("WIFI_PASSWORD");
+
+    let wifi_sta_device = interfaces.sta;
+
+    let sta_config = embassy_net::Config::dhcpv4(Default::default());
+
+    let station_config = esp_radio::wifi::ModeConfig::Client(
+        esp_radio::wifi::ClientConfig::default()
+            .with_ssid(SSID.into())
+            .with_password(PASSWORD.into()),
+    );
+    wifi_controller.set_config(&station_config).unwrap();
+
+    let rng = esp_hal::rng::Rng::new();
+    let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+
+    let (net_stack, net_runner) =
+        embassy_net::new(wifi_sta_device, sta_config, NETWORK_RESOURCES.take(), seed);
+
+    spawner.spawn(wifi_task(wifi_controller)).unwrap();
+    spawner.spawn(net_task(net_runner)).unwrap();
+
+    println!("Waiting for network link...");
+    net_stack.wait_link_up().await;
+    println!("Link up, waiting for config up");
+    net_stack.wait_config_up().await;
+    println!("Network config up! {:?}", net_stack.config_v4());
+
+    let image_bytes = get_image_data(net_stack).await;
+
+    let cursor = ZCursor::new(image_bytes);
+    let mut decoder = JpegDecoder::new(cursor);
+
+    let pixels = decoder.decode().expect("is fucked");
+    let img_info = decoder.info().expect("Missing JPEG info");
+
+    let mut hex_pixels = alloc::vec::Vec::<u8>::new();
+
+    use core::slice::ChunksExact;
+    let image_chunks: ChunksExact<'_, u8> = pixels.chunks_exact(3);
+
+    for rgb in image_chunks {
+        let rgb888 = Rgb888::new(rgb[0], rgb[1], rgb[2]);
+        let hex_color: HexColor = rgb888.into();
+
+        hex_pixels.push(hex_color.get_nibble());
+    }
+
+    println!("image size: {:?}x{:?}", img_info.width, img_info.height);
+
+    let raw = ImageRaw::<HexColor>::new(&hex_pixels, img_info.width as u32);
+    let image = Image::new(&raw, Point::zero());
+
+    let epd_spi_bus = Spi::new(
+        peripherals.SPI2,
+        esp_hal::spi::master::Config::default()
+            .with_frequency(esp_hal::time::Rate::from_mhz(20))
+            .with_mode(esp_hal::spi::Mode::_0),
+    )
+    .unwrap()
+    .with_sck(peripherals.GPIO7)
+    .with_mosi(peripherals.GPIO8)
+    .with_mosi(peripherals.GPIO9);
+
+    info!("Bus ");
+
+    let mut delay = Delay::new();
+
+    let screen_cs = Output::new(peripherals.GPIO10, Level::Low, OutputConfig::default());
+    let screen_dc = Output::new(peripherals.GPIO11, Level::Low, OutputConfig::default());
+    let screen_rst = Output::new(peripherals.GPIO12, Level::Low, OutputConfig::default());
+    let screen_busy = Input::new(
+        peripherals.GPIO13,
+        InputConfig::default().with_pull(Pull::Up),
+    );
+
+    let mut epd_spi_dev = ExclusiveDevice::new(epd_spi_bus, screen_cs, delay).unwrap();
+
+    info!("Screen pins");
+
+    let mut epd7in3e = Epd7in3e::new(
+        &mut epd_spi_dev,
+        screen_busy,
+        screen_dc,
+        screen_rst,
+        &mut delay,
+        None,
+    )
+    .unwrap();
+
+    let mut display = Display7in3e::default();
+
+    // embedded_graphics::primitives::Rectangle::new(Point::new(0, 0), Size::new(999, 999))
+    //     .into_styled(embedded_graphics::primitives::PrimitiveStyle::with_fill(
+    //         HexColor::Yellow,
+    //     ))
+    //     .draw(&mut display)
+    //     .unwrap();
+
+    image.draw(&mut display).unwrap();
+
+    epd7in3e
+        .update_and_display_frame(&mut epd_spi_dev, display.buffer(), &mut delay)
+        .unwrap();
+
+    epd7in3e.sleep(&mut epd_spi_dev, &mut delay).unwrap();
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(60)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
+}
+
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, esp_radio::wifi::WifiDevice<'static>>) {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn wifi_task(mut controller: esp_radio::wifi::WifiController<'static>) {
+    println!("Start connection task");
+    println!("Device capabilities: {:?}", controller.capabilities());
+
+    println!("Starting WiFi");
+    controller.start_async().await.unwrap();
+    println!("Wifi started");
+    loop {
+        println!("Connecting WiFi");
+        match controller.connect_async().await {
+            Ok(_) => {
+                println!("Connected");
+                controller
+                    .wait_for_event(esp_radio::wifi::WifiEvent::StaDisconnected)
+                    .await;
+                println!("Disconnected");
+            }
+            Err(e) => {
+                println!("Failed to connect to wifi: {:?}", e);
+                println!("Retry in 5sec");
+                Timer::after(Duration::from_secs(5)).await;
+            }
+        }
+    }
+}
+
+static NETWORK_RESOURCES: static_cell::ConstStaticCell<embassy_net::StackResources<4>> =
+    static_cell::ConstStaticCell::new(embassy_net::StackResources::new());
+
+static RADIO_CONTROLLER: static_cell::StaticCell<esp_radio::Controller> =
+    static_cell::StaticCell::new();
+
+use embedded_io_async::BufRead;
+async fn get_image_data<'t>(stack: embassy_net::Stack<'t>) -> alloc::vec::Vec<u8> {
+    // DNS Client
+    let dns = embassy_net::dns::DnsSocket::new(stack);
+
+    // TCP state
+    let tcp_state = embassy_net::tcp::client::TcpClientState::<1, 4096, 4096>::new();
+    let tcp = embassy_net::tcp::client::TcpClient::new(stack, &tcp_state);
+
+    println!("Attempting to do HTTP request");
+
+    let mut write_buffer = [0u8; 4096];
+    let mut read_buffer = [0u8; 16640];
+    let config = TlsConfig::new(
+        69420,
+        &mut read_buffer,
+        &mut write_buffer,
+        reqwless::client::TlsVerify::None,
+    );
+
+    let mut http_client = reqwless::client::HttpClient::new_with_tls(&tcp, &dns, config);
+
+    // const URL: &str = env!("WIFI_URL");
+    let url = "https://makeameme.org/media/templates/mocking-spongebob.jpg";
+    // let url = "https://makeameme.org/media/templates/happy_homer.jpg";
+    // let url = "https://c.tadst.com/gfx/600x337/rainbow.jpg?1";
+
+    let mut request = http_client
+        .request(reqwless::request::Method::GET, url)
+        .await
+        .unwrap();
+    println!("HTTP request done?");
+
+    let mut http_rx_buf = [0u8; 4096];
+    let mut response = request
+        .send(&mut http_rx_buf)
+        .await
+        .unwrap()
+        .body()
+        .reader();
+    println!("Reading body");
+
+    let mut body = alloc::vec::Vec::new();
+    loop {
+        let chunk = response.fill_buf().await.unwrap();
+        if chunk.is_empty() {
+            break;
+        }
+        body.extend_from_slice(chunk);
+        let len = chunk.len();
+        response.consume(len);
+    }
+    println!("Got body");
+    body
+}
